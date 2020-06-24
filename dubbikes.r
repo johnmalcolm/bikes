a0cc0bceaa14867c935c355fb8ec5e326178ab94
John Malcolm Anderson <jmanderson@verizonmedia.com>
2020-06-24 09:38:18 +0100

    colour codes

--- dubbikes.r
+++ dubbikes.r
@@ -4,12 +4,18 @@ library('ggplot2')
 library('datetime')
 
 bikes_df <- read.csv('/Users/jmanderson/Downloads/dublinbikes_20180701_20181001.csv')
+str(bikes_10_df)
 
-bikes_10_df <- bikes_df[0:10,]
+bikes_10_df <- bikes_df[0:2000,]
 
-datetimes <- as.POSIXct(as.character(bikes_10_df$TIME), format='%Y-%m-%d %H:%M:%S')
+datetimes <- as.POSIXct(as.character(bikes_10_df$TIME), format='%d/%m/%Y %H:%M')
+
 bikes_10_df$datetimes <- datetimes
 
-ggplot(aes(x = datetimes, y = AVAILABLE.BIKES), data = bikes_10_df) + geom_point()+
-  scale_x_datetime(date_labels = "%H:%M")
+ggplot(aes(x = datetimes, y =AVAILABLE.BIKES, colour=ADDRESS), data = bikes_10_df) + geom_point() +
+  scale_x_datetime(date_labels = "%H:%M") + guides(colour=FALSE) + geom_line(aes(group = ADDRESS))
+
+
+
+
 
